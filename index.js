9ec5b1f0497cecaf8554c9af77470ab467201c66
romanzrespicio <roman.respicio05@gmail.com>
2026-04-24 21:52:07 -0700

    lab 4 fix pt 2

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 
-const projects = await fetchJSON('./lib/projects.json');
-const latestProjects = projects.slice(0, 3);
-
+const profileStats = document.querySelector('#profile-stats');
 const projectsContainer = document.querySelector('.projects');
 
-renderProjects(latestProjects, projectsContainer, 'h2');
+try {
+  const projects = await fetchJSON('./lib/projects.json');
+  const latestProjects = projects.slice(0, 3);
+  renderProjects(latestProjects, projectsContainer, 'h2');
+} catch {
+  if (projectsContainer) {
+    projectsContainer.textContent =
+      'Could not load projects data. Make sure you are viewing the site via a server (not a file:// URL).';
+  }
+}
 
-const githubData = await fetchGitHubData('romanzrespicio');
+try {
+  const githubData = await fetchGitHubData('romanzrespicio');
 
-const profileStats = document.querySelector('#profile-stats');
-
-if (profileStats) {
-  profileStats.innerHTML = `
-    <dl>
-      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-      <dt>Followers:</dt><dd>${githubData.followers}</dd>
-      <dt>Following:</dt><dd>${githubData.following}</dd>
-    </dl>
-  `;
+  if (profileStats) {
+    profileStats.innerHTML = `
+      <dl>
+        <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+        <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+        <dt>Followers:</dt><dd>${githubData.followers}</dd>
+        <dt>Following:</dt><dd>${githubData.following}</dd>
+      </dl>
+    `;
+  }
+} catch {
+  if (profileStats) {
+    profileStats.textContent =
+      'Could not load GitHub stats right now (API blocked or rate-limited).';
+  }
 }
 
